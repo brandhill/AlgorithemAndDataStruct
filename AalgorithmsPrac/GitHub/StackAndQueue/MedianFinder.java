25ce2b2f94d6c30790d2a03ddbfb33e549e9c55e
harsha <hrshchaitanya@gmail.com>
2016-12-28 15:42:04 -0800

    Median Finder Problem

---
+++ AalgorithmsPrac/GitHub/StackAndQueue/MedianFinder.java
@@ -0,0 +1,62 @@
+package StackAndQueue;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MedianFinder {
+	PriorityQueue<Integer> max = new PriorityQueue<Integer>(Collections.reverseOrder());
+	PriorityQueue<Integer> min = new PriorityQueue<Integer>();
+	
+	void addNum(int num) {
+		if(max.isEmpty()) {
+			max.add(num);
+		}
+		else if(max.size()-min.size()>=1) {
+			if(num>max.peek()) {
+				min.add(num);
+			}
+			else {
+				min.add(max.poll());
+				max.add(num);
+			}
+		}
+		else {
+			if(num>min.peek()) {
+				max.add(min.poll());
+				min.add(num);
+			}
+			else {
+				max.add(num);
+			}
+		}
+	}
+	
+	double findMedian() {
+		double ans;
+		if(max.size()==min.size()) {
+			ans = ((double)max.peek()+(double)min.peek())/2;
+		}
+		else {
+			ans = (double) max.peek();
+		}
+		return ans;
+	}
+	
+	public static void main(String[] args) {
+		MedianFinder mf = new MedianFinder();
+        mf.addNum(4);
+        System.out.println(mf.findMedian());
+        mf.addNum(8);
+        System.out.println(mf.findMedian());
+        mf.addNum(2);
+        System.out.println(mf.findMedian());
+        mf.addNum(11);
+        System.out.println(mf.findMedian());
+        mf.addNum(13);
+        System.out.println(mf.findMedian());
+        mf.addNum(14);
+        System.out.println(mf.findMedian());
+        mf.addNum(-1);
+        System.out.println(mf.findMedian());
+	}
+}
